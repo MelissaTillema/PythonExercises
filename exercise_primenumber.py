6be71dabf041bf667eb6fe3625ab5b18009141f8
MelissaTillema <mtillema427@gmail.com>
2026-02-10 18:50:18 +0100

    change in range

--- exercise_primenumber.py
+++ exercise_primenumber.py
@@ -1,17 +1,16 @@
 #!/bin/python3
 
 import random
-x=random.randint(2,10000)
 
 def isprime(x):
     if x < 2:
         return False
-    for i in range(2, int(x**0.5) + 1):
+    for i in range(2, x):
         if x % i == 0:
             return False
     return True
 
-for b in range(5):
+for _ in range(5):
     x=random.randint(2,10000)
     if isprime(x):
         print(x, "is a prime number")
