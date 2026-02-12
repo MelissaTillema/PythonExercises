5b442685c6f66596b233961c171584160855b046
MelissaTillema <mtillema427@gmail.com>
2026-02-12 20:15:07 +0100

    initial upload

---
+++ assignment_collatz.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+# Author: Melissa
+
+def collatz(number):
+    # Make a list
+    numbers = list()
+
+    # Make a iseven function to detect even or odd
+    def iseven(number):
+        if number%2 == 1: #odd
+            return False  
+        else:
+            return True
+    
+    # Put number in the list
+    numbers.append(number)
+
+    # Collatz sequence
+    while True: 
+        if iseven(number) == False:
+            number = number * 3 + 1
+            numbers.append(number)
+        else:
+            number = int(number / 2)
+            numbers.append(number)
+        if number == 1:
+            return numbers
+
+#print the list without brackets and with comma
+def printalista(numbers):
+    print(*numbers, sep=", ")
+
+x=int(input("Skriv ett tal: "))
+listvariabel=collatz(x)
+printalista (listvariabel)
