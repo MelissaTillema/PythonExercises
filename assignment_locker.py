2e4ad3819ccc9bd00712f346845c40d8a94425a2
MelissaTillema <mtillema427@gmail.com>
2026-02-18 19:04:15 +0100

    did the assignment

--- assignment_locker.py
+++ assignment_locker.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+# Author: Jimmy
+# Editor: Melissa
 
 antal=10000
-
-skap = "S" * (antal + 1)
 
 # S = Stänga, O = öppna
 action = "S"
@@ -12,8 +12,10 @@ for elevnr in range(1, antal+1):
 
     # Loopa igenom alla skåp
     for skapnr in range(0, antal + 1, elevnr):
+        if skap[skapnr] == "S":
+            action = "O"
+        else:
+            action = "S"
         skap = skap[:skapnr] + action + skap[skapnr+1:]
 
-    # Ändra action
-    action = "S" if action == "O" else "O"
 print(f"{skap[1:].count('O')} skåp är öppna")
