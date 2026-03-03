06687fbda1125314dd1252f9cbf9c5fe006a1863
MelissaTillema <mtillema427@gmail.com>
2026-03-03 19:01:22 +0100

    changed identation

--- exercise_netdata_api.py
+++ exercise_netdata_api.py
@@ -9,18 +9,18 @@ servers = [148, 149, 150]
 for server in servers:
     url = f"http://10.0.5.{server}:19999/api/v1/info"
     while True:
-            response = requests.get(url, timeout=5)
-            data = response.json()  # Convert response to JSON
+        response = requests.get(url, timeout=5)
+        data = response.json()  # Convert response to JSON
 
-            # Extract the values you want
-            os_name = data.get("os_name")
-            os_version = data.get("os_version")
-            mirrored_hosts = data.get("mirrored_hosts")
+        # Extract the values you want
+        os_name = data.get("os_name")
+        os_version = data.get("os_version")
+        mirrored_hosts = data.get("mirrored_hosts")
 
-            # Print nicely
-            print(f"Server 10.0.5.{server}:")
-            print(f"  OS Name: {os_name}")
-            print(f"  OS Version: {os_version}")
-            print(f"  Mirrored Hosts: {mirrored_hosts[0]}")
-            print(" ")
-            break
+        # Print nicely
+        print(f"Server 10.0.5.{server}:")
+        print(f"  OS Name: {os_name}")
+        print(f"  OS Version: {os_version}")
+        print(f"  Mirrored Hosts: {mirrored_hosts[0]}")
+        print(" ")
+        break
